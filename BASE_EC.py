eea06cf21aa147919c309bee952ec9a7d7029223
Sam-1-cmd <ange.brou@estp.fr>
2025-05-13 16:02:17 +0200

    Add files via upload

---
+++ BASE_EC.py
@@ -0,0 +1,11 @@
+
+print('hello")
+
+#step 1: Import the libraries
+import streamlit as st
+import pandas as pd
+
+#step 2: Set the title of the app
+st.title('Page analyse Base E+C- empreinte carbone')
+
+#Step 3: file uploader
