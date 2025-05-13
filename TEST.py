d65e41f127a304d17b4d8e2abf38cfb469262507
Sam-1-cmd <ange.brou@estp.fr>
2025-05-13 15:07:32 +0200

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,46 @@
+#cd C:\Users\anges\.anaconda
+#python streamlit_tp.py
+#streamlit run TEST.py
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+st.write("Hello, world! This is a Streamlit app.")
+
+st.title("📁 – FolderMy Streamlit App")
+st.subheader("Try out the app")
+st.text("this is a simple text element")
+
+#2 Sidebar
+#choix dans une liste deroulante (dans la sidebar )
+graph_type = st.sidebar.selectbox("coisissez un type de graphique :", ["ligne","barres", "Aucun"])
+
+st.write(f"Vous avez choisi le type de graphique :{graph_type}")
+
+#3 Uploading a Cv
+uploaded_file = st.file_uploader("Télechargez un fichier CSV", type=["csv"])
+if uploaded_file is not None:
+
+    df = pd.read_csv(uploaded_file)
+    st.write("Voici un aperçu de votre fichier :")
+    st.dataframe(df.head())
+
+    #4 Affichage du graphique en fonction du type choisi
+    if graph_type == "Ligne":
+        st.line_chart(df)
+    elif graph_type == "Barres":
+        st.bar_chart(df)
+    else:
+        st.write("Aucun graphique sélectionné.")
+
+#4 SLider
+age = st.slider("Quel âge avez-vous ?", 0, 100, 25)
+st.write(f"Vous avez {age} ans.")
+
+# Checkbox
+if st.checkbox("Afficher un tableau aleatoire"):
+st.write(pd.DataFrame(np.random.randn(5, 3), columns=['A', 'B', 'C']
+
+st.write("Merci d'avoir utilisé le site internet de Samuel")
+
