10232c4f890354a22b6726bf42bc2d549eb4dd2e
Sam-1-cmd <ange.brou@estp.fr>
2025-05-13 16:06:50 +0200

    Update BASE_EC.py

--- BASE_EC.py
+++ BASE_EC.py
@@ -1,6 +1,3 @@
-
-print('hello")
-
 #step 1: Import the libraries
 import streamlit as st
 import pandas as pd
@@ -9,3 +6,6 @@ import pandas as pd
 st.title('Page analyse Base E+C- empreinte carbone')
 
 #Step 3: file uploader
+uploaded_file = st.file_uploader("Importez le fichier Excel E+C- ", type=["csv"])
+df_raw = pd.read_excel(uploaded_file, sheet_name='batiments', header_name="")
+st.dataframe(df_raw.head(3))
