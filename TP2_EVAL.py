b5c2f49f3bcbd20bd93f8823713417732c46ecca
Sam-1-cmd <ange.brou@estp.fr>
2025-05-20 16:06:20 +0200

    Update TP2_EVAL.py

--- TP2_EVAL.py
+++ TP2_EVAL.py
@@ -10,7 +10,21 @@ import pandas as pd
 
 st.title("Mon application TP2")
 st.subheader("Try out the app")
-st.text("this is a simple text element")
+
+st.markdown(
+    """
+    <div style="position: relative; text-align: center; color: white;">
+        <img src="https://cdn.futura-sciences.com/buildsv6/images/largeoriginal/c/3/d/c3d36fbae8_127078_ingenieurs-transverses-zinkevych-fotolia.jpg" style="width: 100%; height: auto; border-radius: 10px;" />
+        <h1 style="position: absolute; top: 50%; left: 50%; 
+                   transform: translate(-50%, -50%); 
+                   background-color: rgba(0, 0, 0, 0.5); 
+                   padding: 20px; border-radius: 10px;">
+            Bienvenue sur mon application Streamlit 🚀
+        </h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 #Demander le nom de l'utilisateur et le saluer
 nom = st.text("Quel est votre nom ?")
