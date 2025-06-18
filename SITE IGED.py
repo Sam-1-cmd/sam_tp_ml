50a063b5bcef7c00a899b7e044002a9130fd715f
Sam-1-cmd <ange.brou@estp.fr>
2025-06-18 11:03:15 +0200

    Update SITE IGED.py

--- SITE IGED.py
+++ SITE IGED.py
@@ -157,7 +157,7 @@ def home_page():
             "ANGE B., 17 ans",
             "Élève en Terminale D",
             "Avec IGED, ma moyenne en maths est passée de 8 à 17 en 3 mois !",
-            "https://imgur.com/a/XkoRrf7"
+            "https://i.imgur.com/7xYheDg.jpeg"
 
         )
         
